a8038924132f76581d96d65f138b3b43a51e16d1
MozzVader <mozz05@gmail.com>
2026-03-05 18:29:06 -0300

    Delete utils directory

--- utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const codeSample = `function greet(name: string): string {
-  return \`Hello, \${name}!\`;
-}
-
-const message: string = greet("World");
-console.log(message);
-
-// This is a sample code snippet
-// demonstrating TypeScript syntax`;
